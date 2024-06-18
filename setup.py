31e665523699a93a7d153948230cc8e6d56229be
fbublitz <frank.bublitz@die-lautmaler.de>
2024-06-18 14:27:16 +0200

    :bookmark: lautcomponents v1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lautcomponents",
-    version="1.1.1",
+    version="1.2.0",
     author="lautmalers",
     author_email="info@die-lautmaler.com",
     description="lautmaler in house rasa components",
